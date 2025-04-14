f634646ee8f215768f0fb8510e1a76065b98c0f7
DSengooba <dsengooba@gmail.com>
2025-04-14 18:39:21 +0300

    learning javascript

---
+++ variables.js
@@ -0,0 +1,25 @@
+
+let mayname="Daniel"
+console.log(mayname)
+let myhome="I live at Kasangati"
+console.log(myhome)
+let myhouse="House Number 008"
+console.log(myhouse)
+let myson="Archie"
+console.log(myson)
+let mycar="Subaru"
+console.log(mycar)
+let firstnumber=2008
+console.log(firstnumber)
+let secondnumber=1924
+console.log(secondnumber)
+let thirdnumber=firstnumber+secondnumber
+console.log(thirdnumber)
+const dateofbirth="August 18 is my birthday"
+console.log(dateofbirth)
+const mygender="I am a Male"
+console.log(mygender)
+const mymother="Joyce is my mother"
+console.log(mymother)
+const christmasday="December 25"
+console.log(christmasday)
